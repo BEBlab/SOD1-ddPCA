f300e485d436dc09f91bb88a3a1ef4b34019e22f
Tomás Quiroga <164334571+tquiroga8@users.noreply.github.com>
2026-06-29 16:55:33 +0200

    Update 05_Create_aPCA_table_scores.R

--- AbundancePCA/05_Create_aPCA_table_scores.R
+++ AbundancePCA/05_Create_aPCA_table_scores.R
@@ -8,4 +8,4 @@ l3_apca <- read.csv("aPCA_L3_table.csv")
 all_apca <- rbind(l1_apca, l2_apca, l3_apca)
 all_apca$low_sigma <- as.character(all_apca$low_sigma)
 
-write.xlsx(all_apca, file = file.path(path_suppl, "Dataset S2_apca.xlsx"), rowNames = FALSE)
+write.xlsx(all_apca, file = "Dataset S2_apca.xlsx", rowNames = FALSE)
